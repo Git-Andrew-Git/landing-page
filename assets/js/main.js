3036cb9fadb53e6c0ec60c5cd3d82c9a461a781c
Git-Andrew-Git <177481666+Git-Andrew-Git@users.noreply.github.com>
2025-03-24 11:18:11 +0100

    Change classe to kebabe-case

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,63 @@
 // funct toggle newslet
 
-const SHOW_NEWSLETTER = document.getElementsByClassName("show__newsletter")
+const SHOW_NEWSLETTER = document.getElementsByClassName("show__newsletter");
 console.log(SHOW_NEWSLETTER);
 
 for (const show of SHOW_NEWSLETTER) {
-    show.addEventListener("click", ()=>{
-        const newsletter = document.querySelector(".newsletter")
-        newsletter.classList.toggle("active")
-        console.log(newsletter);
-        
-    })
+  show.addEventListener("click", () => {
+    const newsletter = document.querySelector(".newsletter");
+    newsletter.classList.toggle("active");
+    console.log(newsletter);
+  });
 }
 
 // décl des élém du compt à rebours
 
-const COUNTDOWN_ELEMENTS = {
-    days: document.getElementById("day"),
-    hours: document.getElementById("hour"),
-    minutes: document.getElementById("minute"),
-    seconds: document.getElementById("second")
-}
+const countdown_ELEMENTS = {
+  days: document.getElementById("day"),
+  hours: document.getElementById("hour"),
+  minutes: document.getElementById("minute"),
+  seconds: document.getElementById("second"),
+};
 
 // fonc pour calculer le compte à rebours
 
-function calculateCountDown(targetDate) {
-    const currentDate = new Date();
-    const timeDifference = targetDate.getTime() - currentDate.getTime();
+function calculatecountdown(targetDate) {
+  const currentDate = new Date();
+  const timeDifference = targetDate.getTime() - currentDate.getTime();
 
-    if(timeDifference <=0) {
-        return {
-            days: 0,
-            hours: 0,
-            minutes: 0,
-            seconds:0,
+  if (timeDifference <= 0) {
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  }
 
-        }
-    }
+  // calcul des jours, h, m, s restantes
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(timeDifference / 60);
+  const days = Math.floor(timeDifference / 24);
 
-    // calcul des jours, h, m, s restantes
-const seconds = Math.floor(timeDifference / 1000)
-const minutes = Math.floor(seconds / 60)
-const hours = Math.floor(timeDifference / 60)
-const days = Math.floor(timeDifference / 24)
-
-return {
+  return {
     days,
     hours: hours % 24,
     minutes: minutes % 60,
-    seconds: seconds % 60
-}
-
+    seconds: seconds % 60,
+  };
 }
 
 // fonction pour mettre à jour le compte à rebours affiché dans la page
-function updateCountdown(countdown) {
-    COUNTDOWN_ELEMENTS.days.textContent = countdown.days;
-    COUNTDOWN_ELEMENTS.hours.textContent = countdown.hours;
-    COUNTDOWN_ELEMENTS.minutes.textContent = countdown.minutes;
-    COUNTDOWN_ELEMENTS.seconds.textContent = countdown.seconds;
+function updatecountdown(countdown) {
+  countdown_ELEMENTS.days.textContent = countdown.days;
+  countdown_ELEMENTS.hours.textContent = countdown.hours;
+  countdown_ELEMENTS.minutes.textContent = countdown.minutes;
+  countdown_ELEMENTS.seconds.textContent = countdown.seconds;
 }
 
 // Date de lancement
-const LAUNCH_DATE = new Date("May 28, 2025 12:00:00")
+const LAUNCH_DATE = new Date("May 28, 2025 12:00:00");
 
 // Interval pour mettre à jour le compte
-
